650d700f1ed0505e899322648f8328951b858a5a
Hongze Zhang <hongze.zhang@intel.com>
2025-01-21 07:50:03 +0800

    fixup

---
+++ src/main/cpp/main/jni_wrapper.cc
@@ -0,0 +1,5 @@
+//
+// Created by root on 1/21/25.
+//
+
+#include "jni_wrapper.h"
---
+++ src/main/cpp/main/jni_wrapper.h
@@ -0,0 +1,10 @@
+//
+// Created by root on 1/21/25.
+//
+
+#ifndef VELOX4JCPP_JNI_WRAPPER_H
+#define VELOX4JCPP_JNI_WRAPPER_H
+
+class jni_wrapper {};
+
+#endif // VELOX4JCPP_JNI_WRAPPER_H
