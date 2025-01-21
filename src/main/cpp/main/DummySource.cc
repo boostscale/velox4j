eca51a4b0579799a82213c4b696b7ac0b15e1de6
Hongze Zhang <hongze.zhang@intel.com>
2025-01-21 09:00:44 +0800

    fixup

--- src/main/cpp/main/DummySource.cc
+++ src/main/cpp/main/DummySource.cc
@@ -1,30 +1,53 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one or more
-* contributor license agreements.  See the NOTICE file distributed with
-* this work for additional information regarding copyright ownership.
-* The ASF licenses this file to You under the Apache License, Version 2.0
-* (the "License"); you may not use this file except in compliance with
-* the License.  You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include "DummySource.h"
 #include <iostream>
+#include "velox/exec/tests/utils/PlanBuilder.h"
 #include "velox/vector/ComplexVector.h"
-#include "velox/core/PlanNode.h"
+#include "velox/vector/tests/utils/VectorTestBase.h"
 
 using namespace facebook::velox;
+using namespace facebook::velox::exec::test;
 
 namespace velox4j {
+class VectorTest : public facebook::velox::test::VectorTestBase {
+ public:
   void foo() {
-    auto vector = BaseVector::create(VARCHAR(), 100, memory::memoryManager()->spillPool());
-    std::cout << "Hello, World! " << vector->toString() << std::endl;
+    std::vector<RowVectorPtr> data{makeRowVector({
+        makeFlatVector<int64_t>({1, 2, 3}),
+        makeFlatVector<int32_t>({10, 20, 30}),
+        makeConstant(true, 3),
+    })};
+
+    auto plan = PlanBuilder()
+                    .values({data})
+                    .partialAggregation({"c0"}, {"count(1)", "sum(c1)"})
+                    .finalAggregation()
+                    .planNode();
   }
+};
+
+void foo() {
+  auto vector =
+      BaseVector::create(VARCHAR(), 100, memory::memoryManager()->spillPool());
+  std::cout << "Hello, World! " << vector->toString() << std::endl;
+
+  VectorTest vectorTest;
+  vectorTest.foo();
 }
+} // namespace velox4j
