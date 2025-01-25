31f552808551c52f1e46e548ba1edd7b75cfa752
Hongze Zhang <hongze.zhang@intel.com>
2025-01-25 18:00:48 +0800

    fixup

--- src/main/java/io/github/zhztheplayer/velox4j/bean/VeloxBeans.java
+++ src/main/java/io/github/zhztheplayer/velox4j/bean/VeloxBeans.java
@@ -2,7 +2,9 @@ package io.github.zhztheplayer.velox4j.bean;
 
 import io.github.zhztheplayer.velox4j.serde.SerdeRegistry;
 import io.github.zhztheplayer.velox4j.serde.SerdeRegistryFactory;
-import io.github.zhztheplayer.velox4j.type.TinyIntType;
+import io.github.zhztheplayer.velox4j.type.*;
+
+import java.math.BigInteger;
 
 public final class VeloxBeans {
   private VeloxBeans() {
@@ -14,6 +16,17 @@ public final class VeloxBeans {
         .registerFactory("Type")
         .key("type");
 
+    typeRegistry.registerClass("BOOLEAN", BooleanType.class);
     typeRegistry.registerClass("TINYINT", TinyIntType.class);
+    typeRegistry.registerClass("SMALLINT", SmallIntType.class);
+    typeRegistry.registerClass("INTEGER", Integer.class);
+    typeRegistry.registerClass("BIGINT", BigInteger.class);
+    typeRegistry.registerClass("HUGEINT", HugeIntType.class);
+    typeRegistry.registerClass("REAL", RealType.class);
+    typeRegistry.registerClass("DOUBLE", DoubleType.class);
+    typeRegistry.registerClass("VARCHAR", VarcharType.class);
+    typeRegistry.registerClass("VARBINARY", VarbinaryType.class);
+    typeRegistry.registerClass("TIMESTAMP", TimestampType.class);
+    typeRegistry.registerClass("ARRAY", ArrayType.class);
   }
 }
