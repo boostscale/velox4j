e9e3a12d81c295f9b9baf2b9654fffec543a22fa
Hongze Zhang <hongze.zhang@intel.com>
2025-01-25 21:48:32 +0800

    fixup

--- src/main/java/io/github/zhztheplayer/velox4j/serde/Serde.java
+++ src/main/java/io/github/zhztheplayer/velox4j/serde/Serde.java
@@ -3,6 +3,7 @@ package io.github.zhztheplayer.velox4j.serde;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
@@ -26,6 +27,7 @@ public final class Serde {
     jsonMapper.disable(MapperFeature.AUTO_DETECT_GETTERS);
     jsonMapper.disable(MapperFeature.AUTO_DETECT_SETTERS);
     jsonMapper.disable(MapperFeature.AUTO_DETECT_CREATORS);
+    jsonMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
     jsonMapper.addModule(new SimpleModule().setDeserializerModifier(new VeloxBeanDeserializer.Modifier()));
     jsonMapper.addModule(new SimpleModule().setSerializerModifier(new VeloxBeanSerializer.Modifier()));
     return jsonMapper.build();
