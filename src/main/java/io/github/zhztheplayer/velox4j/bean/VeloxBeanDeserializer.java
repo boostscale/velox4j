72c8284f33985a5f8753d6f1b9a3f151f6c5c431
Hongze Zhang <hongze.zhang@intel.com>
2025-01-25 17:37:21 +0800

    fixup

--- src/main/java/io/github/zhztheplayer/velox4j/bean/VeloxBeanDeserializer.java
+++ src/main/java/io/github/zhztheplayer/velox4j/bean/VeloxBeanDeserializer.java
@@ -19,10 +19,22 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class VeloxBeanDeserializer extends JsonDeserializer<Object> {
-  private final JsonDeserializer<?> base;
 
-  public VeloxBeanDeserializer(JsonDeserializer<?> base) {
-    this.base = base;
+  public VeloxBeanDeserializer() {
+  }
+
+  private SerdeRegistry findRegistry(SerdeRegistryFactory rf, ObjectNode obj) {
+    final Set<String> keys = rf.keys();
+    final List<String> keysInObj = Streams.fromIterator(obj.fieldNames()).filter(keys::contains).collect(Collectors.toList());
+    if (keysInObj.isEmpty()) {
+      throw new UnsupportedOperationException("Required keys not found in JSON: " + obj);
+    }
+
+    if (keysInObj.size() > 1) {
+      throw new UnsupportedOperationException("Ambiguous key annotations in JSON: " + obj);
+    }
+    final SerdeRegistry registry = rf.key(keysInObj.get(0));
+    return registry;
   }
 
   private Object deserializeWithRegistry(JsonParser p, DeserializationContext ctxt, SerdeRegistry registry, ObjectNode objectNode) {
@@ -31,15 +43,8 @@ public class VeloxBeanDeserializer extends JsonDeserializer<Object> {
     Preconditions.checkArgument(registry.contains(value), "Value %s not registered in registry: %s", value, registry.prefixAndKey());
     if (registry.isFactory(value)) {
       final SerdeRegistryFactory rf = registry.getFactory(value);
-      final Set<String> keys = rf.keys();
-      final List<String> keysInObj = Streams.fromIterator(objectNode.fieldNames()).filter(keys::contains).collect(Collectors.toList());
-      if (keysInObj.isEmpty()) {
-        throw new UnsupportedOperationException("Required keys not found in JSON: " + value);
-      }
-      if (keysInObj.size() > 1) {
-        throw new UnsupportedOperationException("Ambiguous key annotations in JSON: " + value);
-      }
-      return deserializeWithRegistry(p, ctxt, rf.key(keysInObj.get(0)), objectNode);
+      final SerdeRegistry nextRegistry = findRegistry(rf, objectNode);
+      return deserializeWithRegistry(p, ctxt, nextRegistry, objectNode);
     }
     if (registry.isClass(value)) {
       Class<?> clazz = registry.getClass(value);
@@ -56,28 +61,20 @@ public class VeloxBeanDeserializer extends JsonDeserializer<Object> {
   public Object deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
     final TreeNode treeNode = p.readValueAsTree();
     if (!treeNode.isObject()) {
-      return base.deserialize(p, ctxt);
+      throw new UnsupportedOperationException("Not a JSON object: " + treeNode);
     }
-    final ObjectNode obj = (ObjectNode) treeNode;
-    final SerdeRegistryFactory rf = SerdeRegistryFactory.get();
-    final Set<String> keys = rf.keys();
-    final List<String> keysInObj = Streams.fromIterator(obj.fieldNames()).filter(keys::contains).collect(Collectors.toList());
-    if (keysInObj.isEmpty()) {
-      return base.deserialize(p, ctxt);
-    }
-
-    if (keysInObj.size() > 1) {
-      throw new UnsupportedOperationException("Ambiguous key annotations in JSON: " + obj);
-    }
-
-    final SerdeRegistry registry = rf.key(keysInObj.get(0));
-    return deserializeWithRegistry(p, ctxt, registry, obj);
+    final ObjectNode objNode = (ObjectNode) treeNode;
+    final SerdeRegistry registry = findRegistry(SerdeRegistryFactory.get(), objNode);
+    return deserializeWithRegistry(p, ctxt, registry, objNode);
   }
 
   public static class Modifier extends BeanDeserializerModifier {
     @Override
     public JsonDeserializer<?> modifyDeserializer(DeserializationConfig config, BeanDescription beanDesc, JsonDeserializer<?> deserializer) {
-      return new VeloxBeanDeserializer(deserializer);
+      if (VeloxBean.class.isAssignableFrom(beanDesc.getBeanClass())) {
+        return new VeloxBeanDeserializer();
+      }
+      return deserializer;
     }
   }
 }
