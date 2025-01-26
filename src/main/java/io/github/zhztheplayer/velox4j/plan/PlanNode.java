acf48ff94596879566050817f287e7a3e146a0f4
Hongze Zhang <hongze.zhang@intel.com>
2025-01-26 13:35:22 +0800

    fixup

--- src/main/java/io/github/zhztheplayer/velox4j/plan/PlanNode.java
+++ src/main/java/io/github/zhztheplayer/velox4j/plan/PlanNode.java
@@ -1,11 +1,17 @@
 package io.github.zhztheplayer.velox4j.plan;
 
-public abstract class PlanNode {
-  private final String name;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import io.github.zhztheplayer.velox4j.bean.VeloxBean;
+
+public abstract class PlanNode extends VeloxBean {
   private final String id;
 
-  protected PlanNode(String name, String id) {
-    this.name = name;
+  protected PlanNode(String id) {
     this.id = id;
+  }
+
+  @JsonGetter("id")
+  public String getId() {
+    return id;
   }
 }
