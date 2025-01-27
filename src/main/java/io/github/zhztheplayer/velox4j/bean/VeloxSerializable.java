3a0b347c96bf26a2844bb3ca0f23aec009102cde
Hongze Zhang <hongze.zhang@intel.com>
2025-01-27 10:31:54 +0800

    fixup

--- src/main/java/io/github/zhztheplayer/velox4j/bean/VeloxSerializable.java
+++ src/main/java/io/github/zhztheplayer/velox4j/bean/VeloxSerializable.java
@@ -3,7 +3,7 @@ package io.github.zhztheplayer.velox4j.bean;
 import io.github.zhztheplayer.velox4j.serde.NativeBean;
 
 /**
- * Java binding of Velox's ISerializable API. A VeloxBean can be serialized to JSON and
+ * Java binding of Velox's ISerializable API. A VeloxSerializable can be serialized to JSON and
  * deserialized from JSON.
  */
 public abstract class VeloxSerializable implements NativeBean {
