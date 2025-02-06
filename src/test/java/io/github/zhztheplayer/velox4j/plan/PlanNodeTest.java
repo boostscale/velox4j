ab32b69c562919061abd2aa165c7ed6fba7dbdce
Hongze Zhang <hongze.zhang@intel.com>
2025-02-06 22:01:10 +0800

    fixup

--- src/test/java/io/github/zhztheplayer/velox4j/plan/PlanNodeTest.java
+++ src/test/java/io/github/zhztheplayer/velox4j/plan/PlanNodeTest.java
@@ -1,6 +1,8 @@
 package io.github.zhztheplayer.velox4j.plan;
 
 import io.github.zhztheplayer.velox4j.Velox4j;
+import io.github.zhztheplayer.velox4j.aggregate.Aggregate;
+import io.github.zhztheplayer.velox4j.aggregate.AggregateStep;
 import io.github.zhztheplayer.velox4j.connector.Assignment;
 import io.github.zhztheplayer.velox4j.connector.ColumnType;
 import io.github.zhztheplayer.velox4j.connector.FileFormat;
@@ -9,6 +11,8 @@ import io.github.zhztheplayer.velox4j.connector.HiveConnectorSplit;
 import io.github.zhztheplayer.velox4j.connector.HiveTableHandle;
 import io.github.zhztheplayer.velox4j.data.RowVector;
 import io.github.zhztheplayer.velox4j.data.RowVectors;
+import io.github.zhztheplayer.velox4j.expression.CallTypedExpr;
+import io.github.zhztheplayer.velox4j.expression.FieldAccessTypedExpr;
 import io.github.zhztheplayer.velox4j.iterator.UpIterator;
 import io.github.zhztheplayer.velox4j.jni.JniApi;
 import io.github.zhztheplayer.velox4j.query.BoundSplit;
@@ -39,14 +43,14 @@ public class PlanNodeTest {
   }
 
   @Test
-  public void testTableScanNode() {
+  public void testSanity() {
     // TODO: Cleanup the code.
     // TODO: Add assertions.
     final JniApi jniApi = JniApi.create();
     final File file = Resources.copyResourceToTmp("data/tpch-sf0.1/nation/nation.parquet");
     final RowType outputType = new RowType(List.of("n_nationkey", "n_name", "n_regionkey", "n_comment"),
         List.of(new BigIntType(), new VarCharType(), new BigIntType(), new VarCharType()));
-    final PlanNode node = new TableScanNode(
+    final TableScanNode scanNode = new TableScanNode(
         "id-1",
         outputType,
         new HiveTableHandle(
@@ -62,7 +66,7 @@ public class PlanNodeTest {
     );
     final List<BoundSplit> splits = List.of(
         new BoundSplit(
-            node.getId(),
+            scanNode.getId(),
             -1,
             new HiveConnectorSplit(
                 "connector-hive",
@@ -84,7 +88,26 @@ public class PlanNodeTest {
             )
         )
     );
-    final Query query = new Query(node, splits);
+    final AggregationNode aggregationNode = new AggregationNode("id-2", AggregateStep.SINGLE,
+        List.of(FieldAccessTypedExpr.create(new BigIntType(), "n_regionkey")),
+        List.of(),
+        List.of("cnt"),
+        List.of(new Aggregate(
+            new CallTypedExpr(new BigIntType(), List.of(
+                FieldAccessTypedExpr.create(new BigIntType(), "n_nationkey")),
+                "sum"),
+            List.of(new BigIntType()),
+            null,
+            List.of(),
+            List.of(),
+            false
+        )),
+        false,
+        List.of(scanNode),
+        null,
+        List.of()
+    );
+    final Query query = new Query(aggregationNode, splits);
     final String queryJson = Serde.toPrettyJson(query);
     final UpIterator itr = jniApi.executeQuery(queryJson);
     final BufferAllocator alloc = new RootAllocator();
