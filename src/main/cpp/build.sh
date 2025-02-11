13083b25b3c6e8787761143f03c7ed647246a5c5
Hongze Zhang <hongze.zhang@intel.com>
2025-02-11 21:47:51 +0800

    fixup

--- src/main/cpp/build.sh
+++ src/main/cpp/build.sh
@@ -10,5 +10,6 @@ NUM_THREADS=$(nproc)
 SOURCE_DIR=$BASEDIR
 BUILD_DIR=$BASEDIR/build
 
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -S "$SOURCE_DIR" -B "$BUILD_DIR"
-cmake --build "$BUILD_DIR" --target velox4j-bundled -j "$NUM_THREADS"
+cmake -DCMAKE_BUILD_TYPE=Debug -S "$SOURCE_DIR" -B "$BUILD_DIR"
+cmake --build "$BUILD_DIR" --target velox4j-shared -j "$NUM_THREADS"
+cmake --install "$BUILD_DIR" --component velox4j --prefix "$BUILD_DIR/install"
