87719426d2d6bda1d7b3963a01493115b9b286e9
Hongze Zhang <hongze.zhang@intel.com>
2025-02-11 21:48:40 +0800

    fixup

--- src/main/cpp/build.sh
+++ src/main/cpp/build.sh
@@ -12,4 +12,4 @@ BUILD_DIR=$BASEDIR/build
 
 cmake -DCMAKE_BUILD_TYPE=Debug -S "$SOURCE_DIR" -B "$BUILD_DIR"
 cmake --build "$BUILD_DIR" --target velox4j-shared -j "$NUM_THREADS"
-cmake --install "$BUILD_DIR" --component velox4j --prefix "$BUILD_DIR/install"
+cmake --install "$BUILD_DIR" --component velox4j --prefix "$BUILD_DIR/dist"
