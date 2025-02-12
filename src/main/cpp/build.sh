d692ee2f7a5939def0af481486a653985a6c8709
Hongze Zhang <hongze.zhang@intel.com>
2025-02-12 12:44:40 +0800

    fixup

--- src/main/cpp/build.sh
+++ src/main/cpp/build.sh
@@ -3,25 +3,25 @@
 set -e
 set -o pipefail
 set -u
-set -x
 
 BASE_DIR=$(dirname $0)
 NUM_THREADS=$(nproc)
 SOURCE_DIR=$BASE_DIR
 BUILD_DIR=$BASE_DIR/build
 INSTALL_DIR=$BUILD_DIR/dist
+INSTALL_LIB_DIR=$INSTALL_DIR/lib
 
 cmake -DCMAKE_BUILD_TYPE=Debug -S "$SOURCE_DIR" -B "$BUILD_DIR"
 cmake --build "$BUILD_DIR" --target velox4j-shared -j "$NUM_THREADS"
 cmake --install "$BUILD_DIR" --component velox4j --prefix "$INSTALL_DIR"
 
-for file in "$INSTALL_DIR"/*
+for file in "$INSTALL_LIB_DIR"/*
 do
   if [ -L "$file" ]
   then
     echo "Found symlink $file in installation directory, try resolving it."
     target=$(readlink -f "$file")
-    if [ "$(dirname "$target")" != "$(readlink -f "$INSTALL_DIR")" ]
+    if [ "$(dirname "$target")" != "$(readlink -f "$INSTALL_LIB_DIR")" ]
     then
       echo "Target $target is not in the same directory as $file."
       exit 1
