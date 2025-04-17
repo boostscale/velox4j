9cee1a398f1b257abe5bbf62979fcf1c127b25ec
Hongze Zhang <hongze.zzz123@gmail.com>
2025-04-17 17:49:51 +0200

    fixup

--- src/main/cpp/main/velox4j/lifecycle/ObjectStore.cc
+++ src/main/cpp/main/velox4j/lifecycle/ObjectStore.cc
@@ -32,7 +32,10 @@ ObjectStore::~ObjectStore() {
     const std::string_view description = (*itr).second;
     ResourceHandle handle = (*itr).first;
     LOG(WARNING)
-        << "Unreleased object found when object store is closing. Store ID: "
+        << "Unclosed object found when object store is closing. Velox4J will"
+           " destroy it automatically but it's recommended to manually close"
+           " the object through the Java API CppObject#close() after use,"
+           " to minimize peak memory pressure of the application. Store ID: "
         << storeId_ << ", Resource handle ID: " << handle
         << ", Description: " << description;
     store_.erase(handle);
