9b78de08db76e6c4bd55559bcdaeced6000f6d2b
Hongze Zhang <hongze.zzz123@gmail.com>
2025-04-26 21:14:54 +0100

    fixup

--- src/main/java/io/github/zhztheplayer/velox4j/iterator/DownIterators.java
+++ src/main/java/io/github/zhztheplayer/velox4j/iterator/DownIterators.java
@@ -3,19 +3,10 @@ package io.github.zhztheplayer.velox4j.iterator;
 import io.github.zhztheplayer.velox4j.data.RowVector;
 
 import java.util.Iterator;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public final class DownIterators {
   public static DownIterator fromJavaIterator(Iterator<RowVector> itr) {
     return new FromJavaIterator(itr);
-  }
-
-  // Deprecated: Use ExternalStreams.newBlockingQueue() instead.
-  @Deprecated
-  public static DownIterator fromBlockingQueue(BlockingQueue<RowVector> queue) {
-    return new FromBlockingQueue(queue);
   }
 
   private static class FromJavaIterator extends BaseDownIterator {
@@ -46,57 +37,6 @@ public final class DownIterators {
     @Override
     public void close() {
 
-    }
-  }
-
-  private static class FromBlockingQueue extends BaseDownIterator {
-    private final BlockingQueue<RowVector> queue;
-    private final AtomicBoolean closed = new AtomicBoolean(false);
-
-    private RowVector pending = null;
-
-    public FromBlockingQueue(BlockingQueue<RowVector> queue) {
-      this.queue = queue;
-    }
-
-    @Override
-    public State advance0() {
-      if (pending != null) {
-        return State.AVAILABLE;
-      }
-      if (queue.isEmpty()) {
-        return State.BLOCKED;
-      }
-      return State.AVAILABLE;
-    }
-
-    @Override
-    public void waitFor() throws InterruptedException {
-      while (true) {
-        if (pending != null) {
-          return;
-        }
-        if (closed.get()) {
-          Thread.currentThread().interrupt();
-          throw new InterruptedException();
-        }
-        pending = queue.poll(100L, TimeUnit.MILLISECONDS);
-      }
-    }
-
-    @Override
-    public RowVector get0() {
-      if (pending != null) {
-        final RowVector out = pending;
-        pending = null;
-        return out;
-      }
-      return queue.remove();
-    }
-
-    @Override
-    public void close() {
-      closed.compareAndSet(false, true);
     }
   }
 
