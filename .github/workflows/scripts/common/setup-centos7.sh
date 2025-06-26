dd403092e7cd199167a99cef014bbedbc765d0e5
Hongze Zhang <hongze.zzz123@gmail.com>
2025-06-26 13:37:26 +0800

    Update setup-centos7.sh

--- .github/workflows/scripts/common/setup-centos7.sh
+++ .github/workflows/scripts/common/setup-centos7.sh
@@ -112,8 +112,7 @@ fi
 cd /tmp
 mkdir patchelf
 cd patchelf
-wget https://github.com/NixOS/patchelf/releases/download/0.18.0/patchelf-0.18.0-x86_64.tar.gz
-tar -xvf patchelf-0.18.0-x86_64.tar.gz
+wget https://github.com/NixOS/patchelf/releases/download/0.17.2/patchelf-0.17.2-x86_64.tar.gz
+tar -xvf patchelf-0.17.2-x86_64.tar.gz
 ln -s /tmp/patchelf/bin/patchelf /usr/local/bin/patchelf
 patchelf --version
-
