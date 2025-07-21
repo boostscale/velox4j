29c3d97ba255802abb326670034c7229dd260c09
Hongze Zhang <hongze.zzz123@gmail.com>
2025-07-21 13:59:18 +0800

    Download TPC-H dataset in testing (#273)

---
+++ src/test/java/io/github/zhztheplayer/velox4j/test/dataset/TestDataset.java
@@ -0,0 +1,31 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package io.github.zhztheplayer.velox4j.test.dataset;
+
+import java.util.List;
+
+/**
+ * An interface to create dataset for testing purpose. The generic type `FID` stands for the ID type
+ * of all files in the dataset.
+ */
+public interface TestDataset<FID> {
+  /** Lists all files in the dataset. */
+  List<FID> listFiles();
+
+  /** Returns one single test data file by the input file ID. */
+  TestDataFile get(FID fid);
+}
---
+++ src/test/java/io/github/zhztheplayer/velox4j/test/dataset/tpch/TpchTableName.java
@@ -0,0 +1,74 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package io.github.zhztheplayer.velox4j.test.dataset.tpch;
+
+import java.util.List;
+
+import io.github.zhztheplayer.velox4j.type.BigIntType;
+import io.github.zhztheplayer.velox4j.type.DecimalType;
+import io.github.zhztheplayer.velox4j.type.RowType;
+import io.github.zhztheplayer.velox4j.type.VarCharType;
+
+public enum TpchTableName {
+  REGION(
+      "region/region.parquet",
+      new RowType(
+          List.of("r_regionkey", "r_name", "r_comment"),
+          List.of(new BigIntType(), new VarCharType(), new VarCharType()))),
+
+  CUSTOMER(
+      "customer/customer.parquet",
+      new RowType(
+          List.of(
+              "s_suppkey",
+              "s_name",
+              "s_address",
+              "s_nationkey",
+              "s_phone",
+              "s_acctbal",
+              "s_comment"),
+          List.of(
+              new BigIntType(),
+              new VarCharType(),
+              new VarCharType(),
+              new BigIntType(),
+              new VarCharType(),
+              new DecimalType(12, 2),
+              new VarCharType()))),
+
+  NATION(
+      "nation/nation.parquet",
+      new RowType(
+          List.of("n_nationkey", "n_name", "n_regionkey", "n_comment"),
+          List.of(new BigIntType(), new VarCharType(), new BigIntType(), new VarCharType())));
+
+  private final String relativePath;
+  private final RowType schema;
+
+  TpchTableName(String relativePath, RowType schema) {
+    this.relativePath = relativePath;
+    this.schema = schema;
+  }
+
+  public RowType schema() {
+    return schema;
+  }
+
+  public String relativePath() {
+    return relativePath;
+  }
+}
