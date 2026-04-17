e9e2c86e607f6354f87cd638206a8b1e33d8a0de
Hongze Zhang <hongze.zzz123@gmail.com>
2026-04-17 16:50:49 +0800

    fixup

--- src/main/cpp/cmake/patches/velox/apply.cmake
+++ src/main/cpp/cmake/patches/velox/apply.cmake
@@ -1,8 +1,8 @@
 # Applies Velox patches in order.
 # Invoked by FetchContent_Declare's PATCH_COMMAND.
 # Variables: SOURCE_DIR, PATCH_DIR
-foreach(
-  patch IN ITEMS "iconfig-gcc-bug-103186.patch" "fix-s2geometry-absl-dep.patch")
+foreach(patch IN ITEMS "iconfig-gcc-bug-103186.patch"
+                       "fix-s2geometry-absl-dep.patch")
   execute_process(
     COMMAND patch -p1 -i "${PATCH_DIR}/${patch}"
     WORKING_DIRECTORY "${SOURCE_DIR}"
