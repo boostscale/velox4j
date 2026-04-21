d8665e2af6176fa341a4440c5ddb58fe6fa87c97
Hongze Zhang <hongze.zzz123@gmail.com>
2026-04-21 12:55:25 +0800

    fixup

--- .github/workflows/scripts/common/free-runner-disk.sh
+++ .github/workflows/scripts/common/free-runner-disk.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 set -euo pipefail
+
+echo "Disk usage BEFORE cleanup:"
+df -h /
+
+# capture available space in KB
+before=$(df --output=avail / | tail -1)
+
 echo "Removing unwanted software... "
 sudo rm -rf /usr/share/dotnet
 sudo rm -rf /usr/local/lib/android
 sudo rm -rf /opt/ghc
 sudo rm -rf /opt/hostedtoolcache/CodeQL
 sudo docker image prune --all --force > /dev/null
-df -h
+
+echo "Disk usage AFTER cleanup:"
+df -h /
