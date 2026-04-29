d3171563a593b7a58e73d073afc64cfcaea5af2f
Hongze Zhang <hongze.zzz123@gmail.com>
2026-04-30 07:59:31 +0800

    fixup

--- src/main/cpp/cmake/patches/velox/apply.cmake
+++ src/main/cpp/cmake/patches/velox/apply.cmake
@@ -1,7 +1,10 @@
 # Applies Velox patches in order.
 # Invoked by FetchContent_Declare's PATCH_COMMAND.
 # Variables: SOURCE_DIR, PATCH_DIR
-foreach(patch IN ITEMS "iconfig-gcc-bug-103186.patch")
+
+set(PATCHES )
+
+foreach(patch IN LISTS PATCHES)
   execute_process(
     COMMAND patch -p1 -i "${PATCH_DIR}/${patch}"
     WORKING_DIRECTORY "${SOURCE_DIR}"
