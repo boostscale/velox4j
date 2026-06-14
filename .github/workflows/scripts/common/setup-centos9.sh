15f07bbf1428aac54247baeb0b68d6ceed11693d
Hongze Zhang <hongze.zzz123@gmail.com>
2026-06-14 12:33:33 +0100

    Add CentOS 9 support

---
+++ .github/workflows/scripts/common/setup-centos9.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+set -u
+
+# Install essentials.
+dnf install -y maven-openjdk11
+
+# Install patchelf.
+cd /tmp
+mkdir patchelf
+cd patchelf
+wget https://github.com/NixOS/patchelf/releases/download/0.17.2/patchelf-0.17.2-x86_64.tar.gz
+tar -xvf patchelf-0.17.2-x86_64.tar.gz
+ln -s /tmp/patchelf/bin/patchelf /usr/local/bin/patchelf
+patchelf --version
